857f9ba5ef75c3e230bacb6c6e1ab934b0d2ef5b
Andreas Kugel <andreas.kugel@web.de>
2024-03-08 15:56:06 +0100

    update

--- src/demos/motorDriver.py
+++ src/demos/motorDriver.py
@@ -17,8 +17,11 @@ import neopixel
 import micropython
 
 _ioMap = {
+    "model":"stampc3u",
+    "device":2,
+    "btn":9,
     "led":2,
-    "i2c":[], # clock first
+    "i2c":[0,1], # clock first. to be checked
     "motor":[3,4,5,6], # 2 pins per motor
 }
  
@@ -36,68 +39,83 @@ def rgbFill(color):
 ##### init motor outputs
 motor = []
 for p in _ioMap["motor"]:
-    motor.append(machine.Pin(p,machine.Pin.OUT))
+    motor.append({
+        "io":p,
+        "pin":machine.PWM(p,freq=1000,duty=0),
+        "mode":"pwm"
+        }
+    )
 
 print("Motor setting:",motor)
 
-#### globals for motor
+##### globals for motor
 _numMotors = micropython.const(len(motor) // 2)
 _motorSteps = micropython.const(5)
 _motorCtl = bytearray([-128]*_numMotors) # -(steps)..0..(steps)
 _motorCtlShadow = bytearray(_numMotors) # local copy within isr/scheduled task
 _motorSlot = bytearray([1]) # start with 1
 
-# isr: copy motorCtl to schadow, then schedule output task
-def motorDriver(slot: int):
-    global _motorCtlShadow, _motorSlot, motor, _numMotors
-    """ Set all motor IO pins from motorCtlShadow according to current slot"""
-    fmt = "b"*_numMotors
-    ctl = struct.unpack(fmt,_motorCtlShadow)
-    #print(ctl)
-    for idx in range (_numMotors):
-        m = ctl[idx]
-        #print(m)
-        if m == 0:
-            # break
-            motor[idx*2].value(0)
-            motor[idx*2+1].value(0)
-        elif m == -128:
-            # free
-            motor[idx*2].value(1)
-            motor[idx*2+1].value(1)
-        elif abs(m) < slot:
-            # free
-            motor[idx*2].value(1)
-            motor[idx*2+1].value(1)
-        elif m < 0:
-            # reverse
-            motor[idx*2].value(0)
-            motor[idx*2+1].value(1)
+##### we use PWM channels for motor
+# a static "1" cannot be (reliably?) set via PWM
+# maybe the max. dutycycle (1023) is ok 
+# else use deinit followed by out
+
+def setPwm(mot):
+    global motor
+    if motor[mot]["mode"] != "pwm":
+        motor[mot]["pin"] = machine.PWM(motor[mot]["io"],freq=1000,duty=0) 
+        motor[mot]["mode"] = "pwm"
+
+def setHigh(mot):
+    global motor
+    if motor[mot]["mode"] == "pwm":
+        motor[mot]["pin"].deinit()
+        machine.Pin(motor[mot]["io"],machine["pin"].OUT,value=1)
+        motor[mot]["mode"] = "high"
+
+
+def motorDrive(midx,value,brk=True):
+    """ set pins of current motor. value in range -10 .. 10"""
+    if midx >= _numMotors:
+        print("Invalid motor")
+        return
+
+    if value == 0:
+        # break
+        setPwm(midx*2)
+        motor[midx*2]["pin"].duty(0)
+        setPwm(midx*2+1)
+        motor[midx*2+1]["pin"].duty(0)
+    elif value == -128:
+        # free
+        setPwm(midx*2)
+        motor[midx*2]["pin"].duty(1023)
+        setPwm(midx*2+1)
+        motor[midx*2+1]["pin"].duty(1023)
+    elif abs(value > 10):
+        print("Invalid value")
+        return
+    elif value < 0:
+        # reverse
+        if brk:
+            motor[midx*2]["pin"].duty(0)
         else:
-            # forward
-            motor[idx*2].value(1)
-            motor[idx*2+1].value(0)
-
-def motorIsr():
-    """ motor timer interrupt handler
-        copy control for driver
-        increment slot
-        schedule driver
-    """
-    global _motorCtl, _motorCtlShadow, _motorSlot, _motorSteps
-    irqState = machine.disable_irq()
-    micropython.schedule(motorDriver,_motorSlot[0])
-    _motorCtlShadow[:] = _motorCtl # copy
-    if _motorSlot[0] >= _motorSteps:
-        _motorSlot[0] = 1 # else we start with min 2 active cycles
+            motor[midx*2]["pin"].duty(1023)
+        motor[midx*2+1]["pin"].duty(int(abs(value) / 10 * 1023))
     else:
-        _motorSlot[0] += 1
-    machine.enable_irq(irqState)
+        # forward
+        motor[midx*2]["pin"].duty(int(abs(value) / 10 * 1023))
+        if brk:
+            motor[midx*2+1]["pin"].duty(0)
+        else:
+            motor[midx*2+1]["pin"].duty(1023)
 
-#### initialize the motor timer
-_motorTimerId = micropython.const(2) # hw timer id
-# on esp32c 50 and 100Hz seem to be good, 500Hz is unstable
-_motorTimerPeriod = micropython.const(20) # ms period
 
-motorTimer = machine.Timer(_motorTimerId)
-motorTimer.init(period=_motorTimerPeriod, mode=machine.Timer.PERIODIC, callback=lambda t:motorIsr())
+##########
+#p = machine.PWM(3,freq=1000,duty=500)
+#p.deinit()
+#p = machine.Pin(3,machine["pin"].OUT,value=1)
+#p = machine.Pin(3,machine["pin"].OUT,value=0)
+#p = machine.PWM(3,freq=1000,duty=500)
+###########
+
