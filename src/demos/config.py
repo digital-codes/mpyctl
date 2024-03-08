9fdc7589c17f53950965ca4f2626cb6d0097fd6a
Andreas Kugel <andreas.kugel@web.de>
2024-03-08 19:48:15 +0100

    config files

---
+++ src/demos/config.py
@@ -0,0 +1,36 @@
+""" read/write config"""
+import json
+import machine
+import os
+import network
+import bluetooth
+
+_CONF_FILE = "config.json"
+
+files = os.listdir("/")
+
+if _CONF_FILE in files:
+    with open(_CONF_FILE) as f:
+        cfg = json.load(f)
+
+    print("Old config")
+
+else:
+    cfg = {}
+    cfg["io"] = {} # no default io definition 
+    cfg["device"] = -1 # no default device
+    cfg["model"] = "" # no default model
+
+    cfg["id"] = machine.unique_id().hex()
+    o = os.uname()
+    cfg["os"] = {"release":o.release,"machine":o.machine}
+
+    b = bluetooth.BLE()
+    b.active(1)
+    cfg["ble"] = {"key":"123321","addr":b.config("mac")[1].hex()}
+    w = network.WLAN()
+    cfg["wlan"] = {"addr":w.config("mac").hex()}
+
+    print("New config")
+
+print(f"Config: {cfg}")
