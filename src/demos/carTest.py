5322c25b1a53fb82601e02b4de2af6ed190c7039
Andreas Kugel <andreas.kugel@web.de>
2024-03-14 19:10:52 +0100

    cartest devkit-c

---
+++ src/demos/carTest.py
@@ -0,0 +1,127 @@
+import sys
+
+sys.path.append("")
+
+from micropython import const
+
+import uasyncio as asyncio
+import aioble
+import bluetooth
+
+import random
+import struct
+
+import machine
+import neopixel
+
+import micropython
+
+_ioMap = {
+    "model":"devkit-c",
+    "device":3,
+    "btn":0,
+    "led":-1,
+    "i2c":[14,27], # clock first. to be checked
+    "motor":[4,16,18,19,21,5,22,23], # 2 pins per motor. pin 3 not OK, use 5 instead
+}
+ 
+##### initialize built-in stuff first ######
+### LED
+if _ioMap["led"] >= 0:
+    neoPin = _ioMap["led"]
+    p = machine.Pin(neoPin)
+    RGB = neopixel.NeoPixel(p,1)
+else:
+    RGB = None
+
+def rgbFill(color):
+    global RGB
+    if RGB != None:
+        RGB.fill(color)
+        RGB.write()
+
+##### init motor outputs
+motor = []
+for p in _ioMap["motor"]:
+    motor.append({
+        "io":p,
+        "pin":machine.PWM(p,freq=1000,duty=0),
+        "mode":"pwm"
+        }
+    )
+
+print("Motor setting:",motor)
+
+##### globals for motor
+_numMotors = micropython.const(len(motor) // 2)
+_motorSteps = micropython.const(5)
+_motorCtl = bytearray([-128]*_numMotors) # -(steps)..0..(steps)
+_motorCtlShadow = bytearray(_numMotors) # local copy within isr/scheduled task
+_motorSlot = bytearray([1]) # start with 1
+
+##### we use PWM channels for motor
+# a static "1" cannot be (reliably?) set via PWM
+# maybe the max. dutycycle (1023) is ok 
+# else use deinit followed by out
+
+def setPwm(mot):
+    global motor
+    if motor[mot]["mode"] != "pwm":
+        motor[mot]["pin"] = machine.PWM(motor[mot]["io"],freq=1000,duty=0) 
+        motor[mot]["mode"] = "pwm"
+
+def setHigh(mot):
+    global motor
+    if motor[mot]["mode"] == "pwm":
+        motor[mot]["pin"].deinit()
+        machine.Pin(motor[mot]["io"],machine.Pin.OUT,value=1)
+        motor[mot]["mode"] = "high"
+
+
+def motorDrive(midx,value,brk=True):
+    """ set pins of current motor. value in range -10 .. 10"""
+    if midx >= _numMotors:
+        print("Invalid motor")
+        return
+
+    if value == 0:
+        # break
+        setPwm(midx*2)
+        motor[midx*2]["pin"].duty(0)
+        setPwm(midx*2+1)
+        motor[midx*2+1]["pin"].duty(0)
+    elif value == -128:
+        # free
+        setHigh(midx*2)
+        setHigh(midx*2+1)
+    elif abs(value > 10):
+        print("Invalid value")
+        return
+    elif value < 0:
+        # reverse
+        if brk:
+            setPwm(midx*2)
+            motor[midx*2]["pin"].duty(0)
+        else:
+            setHigh(midx*2)
+        setPwm(midx*2+1)
+        motor[midx*2+1]["pin"].duty(int(abs(value) / 10 * 1023))
+    else:
+        # forward
+        setPwm(midx*2)
+        motor[midx*2]["pin"].duty(int(abs(value) / 10 * 1023))
+        if brk:
+            setPwm(midx*2+1)
+            motor[midx*2+1]["pin"].duty(0)
+        else:
+            setHigh(midx*2+1)
+
+
+##########
+#p = machine.PWM(3,freq=1000,duty=500)
+#p.deinit()
+#p = machine.Pin(3,machine.Pin.OUT,value=1)
+#p = machine.Pin(3,machine.Pin.OUT,value=0)
+#p = machine.PWM(3,freq=1000,duty=500)
+###########
+
