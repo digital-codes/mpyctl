e1630f836ef1f0226ef88243d86a66c99e052a8a
Andreas Kugel <andreas.kugel@web.de>
2024-03-15 10:40:48 +0100

    cartest update

--- src/demos/carTest.py
+++ src/demos/carTest.py
@@ -2,33 +2,83 @@ import sys
 
 sys.path.append("")
 
-from micropython import const
+from micropython import const as constant
 
-import uasyncio as asyncio
-import aioble
 import bluetooth
-
-import random
-import struct
+import network 
+import json 
+import os
 
 import machine
 import neopixel
 
-import micropython
 
-_ioMap = {
-    "model":"devkit-c",
-    "device":3,
-    "btn":0,
-    "led":-1,
-    "i2c":[14,27], # clock first. to be checked
-    "motor":[4,16,18,19,21,5,22,23], # 2 pins per motor. pin 3 not OK, use 5 instead
-}
+# activate ble first
+b = bluetooth.BLE()
+b.active(1)
+
+_CONF_FILE = "config.json"
+
+files = os.listdir("/")
+
+if _CONF_FILE in files:
+    with open(_CONF_FILE) as f:
+        cfg = json.load(f)
+    print("Old config:", cfg)
+
+else:
+    _ioMap = {
+        "model":"devkit-c",
+        "device":-1,
+        "btn":0,
+        "led":-1,
+        "i2c":[14,27], # clock first. to be checked
+        "motor":[23,22,21,5,16,4,18,19] # 2 pins per motor. pin 3 not OK, use 5 instead
+        #"motor":[4,16,18,19,21,5,22,23], # 2 pins per motor. pin 3 not OK, use 5 instead
+    }
+    cfg = {}
+    cfg["io"] = _ioMap 
+    cfg["device"] = -1 # no default device
+    cfg["model"] = "" # no default model
+
+    cfg["id"] = machine.unique_id().hex()
+    o = os.uname()
+    cfg["os"] = {"release":o.release,"machine":o.machine}
+
+    bmac = b.config("mac")[1].hex()
+    bkey = f"{(int(bmac, 16) % 1_000_000):06}"
+    cfg["ble"] = {"key":bkey,"addr":bmac}
+    w = network.WLAN()
+    cfg["wlan"] = {"addr":w.config("mac").hex()}
+
+    print("New config")
+
+# update config
+if cfg["id"] != machine.unique_id().hex():
+    print("ID mismatch")
+    cfg["id"] = machine.unique_id().hex()
+    o = os.uname()
+    cfg["os"] = {"release":o.release,"machine":o.machine}
+
+    bmac = b.config("mac")[1].hex()
+    bkey = f"{(int(bmac, 16) % 1_000_000):06}"
+    cfg["ble"] = {"key":bkey,"addr":bmac}
+    w = network.WLAN()
+    cfg["wlan"] = {"addr":w.config("mac").hex()}
+    print("Updating config")
+    with open(_CONF_FILE,"w") as f:
+        json.dump(cfg,f)
+else:
+    print("Config OK")
+
+print(f"Config: {cfg}")
+
+
  
 ##### initialize built-in stuff first ######
 ### LED
-if _ioMap["led"] >= 0:
-    neoPin = _ioMap["led"]
+if cfg["io"]["led"] >= 0:
+    neoPin = cfg["io"]["led"]
     p = machine.Pin(neoPin)
     RGB = neopixel.NeoPixel(p,1)
 else:
@@ -42,7 +92,7 @@ def rgbFill(color):
 
 ##### init motor outputs
 motor = []
-for p in _ioMap["motor"]:
+for p in cfg["io"]["motor"]:
     motor.append({
         "io":p,
         "pin":machine.PWM(p,freq=1000,duty=0),
@@ -53,8 +103,8 @@ for p in _ioMap["motor"]:
 print("Motor setting:",motor)
 
 ##### globals for motor
-_numMotors = micropython.const(len(motor) // 2)
-_motorSteps = micropython.const(5)
+_motorSteps = constant(10)
+_numMotors = constant(len(motor) // 2)
 _motorCtl = bytearray([-128]*_numMotors) # -(steps)..0..(steps)
 _motorCtlShadow = bytearray(_numMotors) # local copy within isr/scheduled task
 _motorSlot = bytearray([1]) # start with 1
@@ -99,17 +149,21 @@ def motorDrive(midx,value,brk=True):
         return
     elif value < 0:
         # reverse
-        if brk:
+        if abs(value) > _motorSteps:
+            value = -_motorSteps
+        if brk: 
             setPwm(midx*2)
             motor[midx*2]["pin"].duty(0)
         else:
             setHigh(midx*2)
         setPwm(midx*2+1)
-        motor[midx*2+1]["pin"].duty(int(abs(value) / 10 * 1023))
+        motor[midx*2+1]["pin"].duty(int(abs(value) / _motorSteps * 1023))
     else:
         # forward
+        if abs(value) > _motorSteps:
+            value = _motorSteps
         setPwm(midx*2)
-        motor[midx*2]["pin"].duty(int(abs(value) / 10 * 1023))
+        motor[midx*2]["pin"].duty(int(abs(value) / _motorSteps * 1023))
         if brk:
             setPwm(midx*2+1)
             motor[midx*2+1]["pin"].duty(0)
--- src/demos/config.py
+++ src/demos/config.py
@@ -7,6 +7,12 @@ import bluetooth
 
 _CONF_FILE = "config.json"
 
+def generate_key(mac_address):
+    """Generate a 6-digit key from a MAC address."""
+    key = int(mac_address, 16)
+    key = key % 1_000_000  # Get the last 6 digits
+    return f"{key:06}"  # Pad with zeros if necessary
+
 files = os.listdir("/")
 
 if _CONF_FILE in files:
@@ -27,7 +33,9 @@ else:
 
     b = bluetooth.BLE()
     b.active(1)
-    cfg["ble"] = {"key":"123321","addr":b.config("mac")[1].hex()}
+    bmac = b.config("mac")[1].hex()
+    bkey = generate_key(bmac)
+    cfg["ble"] = {"key":bkey,"addr":bmac}
     w = network.WLAN()
     cfg["wlan"] = {"addr":w.config("mac").hex()}
 
