647ba9ad9e84ab2abb62b0f95faba7ea36f6aa5a
Andreas Kugel <andreas.kugel@web.de>
2024-03-15 18:02:04 +0100

    dbtest update

--- src/demos/dbtest.py
+++ src/demos/dbtest.py
@@ -1,5 +1,18 @@
+"""create and maintain mpyctl device database"""
 import sqlite3
 from sqlite3 import Error
+import os
+
+# defs
+# Device name
+_devName = "MpyCtl"
+
+# Database file
+_db_dir = "/home/kugel/daten/work/database/mpyctl"
+_db_name = 'devices.db'
+_database = os.sep.join(_db_dir.split("/") + [_db_name])
+print("DB:",_database)
+
 
 # Define function to create connection to SQLite database
 def create_connection(db_file):
@@ -19,6 +32,7 @@ def create_table(conn):
         c = conn.cursor()
         c.execute('''CREATE TABLE IF NOT EXISTS devices
                      (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      name TEXT NOT NULL,
                       address TEXT NOT NULL,
                       config TEXT NOT NULL)''')
     except Error as e:
@@ -26,12 +40,12 @@ def create_table(conn):
 
 
 # Function to insert a new row with MAC address
-def insert_row(conn, json_string, mac_address):
+def insert_row(conn, json_string, mac_address, dev_name):
     try:
-        sql = ''' INSERT INTO devices(config, address)
-                  VALUES(?,?) '''
+        sql = ''' INSERT INTO devices(config, address, name)
+                  VALUES(?,?,?) '''
         cur = conn.cursor()
-        cur.execute(sql, (json_string, mac_address))
+        cur.execute(sql, (json_string, mac_address, dev_name))
         conn.commit()
         return cur.lastrowid
     except Error as e:
@@ -48,24 +62,29 @@ def get_latest_id(conn):
     except Error as e:
         print(e)
 
-# Database file
-db_file = 'devices.db'
 
 # Create a database connection
-conn = create_connection(db_file)
+conn = create_connection(_database)
 
 if conn is not None:
     # Create table
     create_table(conn)
 
     # Insert a new row with JSON string and MAC address
-    json_string = '{"name": "Alice", "age": 28, "city": "Los Angeles"}'
-    mac_address = "00:1B:3C:4D:5E:6F"
-    row_id = insert_row(conn, json_string, mac_address)
+    data = '{"name": "Alice", "age": 28, "city": "Los Angeles"}'
+    address = "00:1B:3C:4D:5E:6F"
+
+    latest_id = get_latest_id(conn)
+    if latest_id == None:
+        latest_id = 0
+    print("Latest:",latest_id)
+
+    name = "_".join([_devName,f"{(latest_id + 1):04}"])
+    row_id = insert_row(conn, data, address,name)
 
     # Retrieve the ID of the latest row
     latest_id = get_latest_id(conn)
-    print("Latest:",latest_id)
+    print("New:",latest_id)
 
     # Close connection
     conn.close()
