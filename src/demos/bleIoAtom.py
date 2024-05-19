6dcd9ae5e77b36e30571989c51d747dfded5a640
Andreas Kugel <andreas.kugel@web.de>
2024-05-19 22:44:27 +0200

    bleIo.py generic. make sure to have led set in config.json

--- src/demos/bleIoAtom.py
+++ src/demos/bleIoAtom.py
@@ -8,120 +8,20 @@ import uasyncio as asyncio
 import aioble
 import bluetooth
 
-import cryptolib
 import random
 import struct
 
 import machine
-import json
-import os
 
-import neopixel
-
-
-# config stuff
-_CONF_FILE = "config.json"
-files = os.listdir("/")
-if _CONF_FILE in files:
-    with open(_CONF_FILE) as f:
-        cfdata = json.load(f)
-else:
-    raise BaseException("No Config")        
-
-# verify id
-if machine.unique_id().hex() != cfdata["id"]:
-    raise BaseException("Invalid ID")        
-
-try:
-    # get ble key
-    _bleKey = cfdata["ble"]["key"]
-    print("blekey:",_bleKey)
-    _ivBase = bytearray([0]*12) # first 3/4 iv
-    _cryptMode = 2 # CBC
-
-    # generate device name
-    _deviceName = f"{cfdata['model']}_{cfdata['device']:04}" 
-    print("Devicename:",_deviceName)
-except:
-    raise BaseException("Invalid Config")        
-
-##### initialize built-in stuff first ######
-### LED
-
-try: 
-    neoPin = cfdata["io"]["led"]
-    p = machine.Pin(neoPin)
-    RGB = neopixel.NeoPixel(p,1)
-except:
-    RGB = None
-
+import atom
+cpu = atom.Matrix()
+# works for matrix and lite
 def rgbFill(color):
-    global RGB
-    if RGB == None:
-        return
-    RGB.fill(color)
-    RGB.write()
+    cpu.set_pixels_color(color[0],color[1],color[2])
 
-
-# pairing stuff
-validation_timer = None
-_msgBytes = const(4)
-pair_value = bytearray([0]*_msgBytes)
-def genChallenge():
-    global pair_value
-    """create new random challenge with 4 digits"""
-    pin = random.getrandbits(8*_msgBytes) # micropython max is 32 bit
-    pair_value = pin.to_bytes(_msgBytes,"big")
-
-def verifyResponse(resp):
-    global _bleKey, _ivBase, _cryptMode
-    global pair_value
-    """verify response data. expect 16byte encrpyted msg followed by 8 byte iv part"""
-    print("Resp:",resp.hex())
-    ivPart = resp[-4:]
-    print(f"IVpart: {ivPart.hex()}")
-    iv = bytearray(_ivBase + ivPart)
-    print(f"IV: {iv.hex()}")
-    print(f"KEY: {_bleKey}")
-    print(f"MODE: {_cryptMode}")
-    crypt = cryptolib.aes(bytes.fromhex(_bleKey),_cryptMode,iv)
-    msg = crypt.decrypt(resp[:16])[:_msgBytes]
-    print(f"DEC: {msg}, {msg.hex()}")
-    print(f"PIN: {pair_value}, {pair_value.hex()}")
-    return msg.hex() == pair_value.hex()
-
-def testChallenge():
-    genChallenge()
-    pin = pair_value
-    print(f"PIN: {pin}")
-
-    ivPart_ = bytearray([random.randint(0,256) for i in range(4)])
-    print(f"IVpart: {ivPart_.hex()}")
-    print(f"IVbase: {_ivBase.hex()}")
-
-    iv = _ivBase + ivPart_
-    print(f"IV: {iv.hex()}")
-
-    fwd = cryptolib.aes(bytes.fromhex(_bleKey),_cryptMode,iv)
-
-    # need to generate 16 byte message for encryption (padding)
-    # standard pkcs7 padding ?
-    msg = pin + bytes([16 - _msgBytes]*(16 - _msgBytes))
-    print(f"MSG: {msg.hex()}")
-
-    encoded = fwd.encrypt(msg)
-    print(f"ENC: {encoded.hex()}")
-
-    response = encoded + ivPart_
-    print(f"RESP: {response.hex()}")
-
-    print(verifyResponse(response))
-
-# testChallenge()
 
 # connected stated
 connected = False
-authorized = False
 currentConnection = None
 
 # machine.unique_id is not stable on Atom S3U ...
@@ -131,20 +31,6 @@ ble = bluetooth.BLE()
 if not ble.active():
     ble.active(True)
 
-
-_IO_CAPABILITY_DISPLAY_ONLY = const(0)
-_IO_CAPABILITY_DISPLAY_YESNO = const(1)
-_IO_CAPABILITY_KEYBOARD_ONLY = const(2)
-_IO_CAPABILITY_NO_INPUT_OUTPUT = const(3)
-_IO_CAPABILITY_KEYBOARD_DISPLAY = const(4)
-
-# test security features
-# ble.config(bond=True,io=_IO_CAPABILITY_DISPLAY_ONLY,le_secure=True)
-# bond=True,io=0,le_secure=True  prompts for pin when run from host application
-# however, this seem to be not processed by aioble
-# ble.config(bond=True,io=_IO_CAPABILITY_NO_INPUT_OUTPUT,le_secure=True)
-useValidation = True
-
 mac = ble.config("mac")
 device_address = mac[1]
 
@@ -152,12 +38,8 @@ device_address_str = ":".join("{:02X}".format(byte) for byte in device_address)
 
 print("BLE Device Address:", device_address_str)
 
-##############################
-# security stuff. unused so far
-#ble.config(bond=True,io=0,le_secure=True)
-# io=0 should be like number compare
 
-DEVICE_NAME = _deviceName
+DEVICE_NAME = "MpyCtlAtom"
 ble.config(gap_name=DEVICE_NAME)
 print("name",DEVICE_NAME)
 
@@ -252,16 +134,6 @@ print("SENSE",ENV_CTL_UUID)
 ENV_CTL_OUT_UUID = bluetooth.UUID(0x2A56)
 print("TEMP",ENV_CTL_OUT_UUID)
 
-# device config characteristics within device information service
-# creating uuid from long string tricky ...
-def mkUuidFromString(s):
-    a = s.replace('-', '')
-    b = bytes.fromhex(a)
-    c = bytes(b[i] for i in range(len(b) - 1, -1, -1))
-    return bluetooth.UUID(c)
-
-DEVICE_CONFIG_RD = mkUuidFromString("19e2282a-0777-4519-9d08-9bc983c3a7d0")
-DEVICE_PAIR = mkUuidFromString("bda7b898-782a-4a50-8d10-79d897ea82c2")
 
 # org.bluetooth.characteristic.gap.appearance.xml
 #_ADV_APPEARANCE_GENERIC_THERMOMETER = const(768)
@@ -299,36 +171,27 @@ mfc_characteristic.write(mfc_name.encode())
 mdl_characteristic = aioble.Characteristic(
     info_service, bluetooth.UUID(0x2A24), read=True, notify=False
 )
-mdl_name = DEVICE_NAME
+mdl_name = "MpyCtl"
 mdl_characteristic.write(mdl_name.encode())
 
-# add config characterisitcs 
-cfg_characteristic = aioble.Characteristic(
-    info_service, DEVICE_CONFIG_RD, read=True, write=False, notify=False
+sno_characteristic = aioble.Characteristic(
+    info_service, bluetooth.UUID(0x2A25), read=True, notify=False
 )
-cfg_value = bytearray([0,1,2,3,4,5,6,7,8,9])
-cfg_characteristic.write(cfg_value)
+sno_name = "1234"
+sno_characteristic.write(sno_name.encode())
 
-# capture must be true in order to get data!!!
-pair_characteristic = aioble.Characteristic(
-    info_service, DEVICE_PAIR, read=True, write=True, notify=False, capture=True
-)
-pair_characteristic.write(pair_value)
+
 
 aioble.register_services(temp_service,ctl_service,info_service)
 
 
 # Helper to encode the temperature characteristic encoding (sint16, hundredths of a degree).
 def _encode_temperature(temp_deg_c):
-    sensData = struct.pack("<hhhbbBB", int(temp_deg_c * 100), int(0), int(1), 2, 3, 4, 5)
-    sensData =  struct.pack("<h", int(temp_deg_c * 100))
-    return sensData
+    return struct.pack("<h", int(temp_deg_c * 100))
 
 def _decode_ctl(data):
     # b: unsigned char
-    print("Len ctl:",len(data))
-    ctlData = struct.unpack("b", data)
-    return ctlData
+    return struct.unpack("b", data)
 
 
 # This would be periodically polling a hardware sensor.
@@ -338,7 +201,7 @@ async def sensor_task():
     print("Start sense")
     t = 24.5
     while True:
-        if connected and authorized:
+        if connected:
             print("Temp",t)
             try:
                 temp_characteristic.write(_encode_temperature(t))
@@ -348,79 +211,37 @@ async def sensor_task():
                 await asyncio.sleep_ms(100)
                 continue
            
-            #t += random.uniform(-0.5, 0.5)
-            t = random.randrange(-300,300) / 10
+            t += random.uniform(-0.5, 0.5)
         await asyncio.sleep_ms(1000)
 
 async def ctl_task():
     global connected
     print("Start ctl")
     while True:
-        if connected and authorized:
+        if connected:
             try:
                 conn, _value = await ctl_characteristic.written()
-                value = _decode_ctl(_value)[0]
-                #print("Received:", value)
-                if value == 1:  # Check if the value is 1 (on)
-                    # Code to turn the light on
-                    print("ON")
-                    rgbFill((0,100,0))
-                elif value == 0:  # Check if the value is 0 (off)
-                    # Code to turn the light off
-                    print("OFF")
-                    rgbFill((0,0,30))
-                else:
-                    print("else")
-                    rgbFill((100,0,0))
             except:
                 print("ctl error")
                 await asyncio.sleep_ms(100)
+                continue
 
+            value = _decode_ctl(_value)[0]
+            print("Received:",conn, value)
+            if value == 1:  # Check if the value is 1 (on)
+                # Code to turn the light on
+                print("ON")
+                rgbFill((0,100,0))
+            elif value == 0:  # Check if the value is 0 (off)
+                # Code to turn the light off
+                print("OFF")
+                rgbFill((0,0,30))
+            else:
+                print("else")
+                rgbFill((100,0,0))
         else:
-            await asyncio.sleep_ms(100)
+            await asyncio.sleep_ms(1000)
 
-async def pair_task():
-    global connected
-    global authorized
-    global validation_timer
-    print("Start pair")
-    while True:
-        if connected:
-            try:
-                conn, _value = await pair_characteristic.written()
-                print("Pair received:",conn,_value)
-                if verifyResponse(_value):
-                    print("Pair OK")
-                    validation_timer.deinit()
-                    authorized = True
-                else:
-                    print("Pair failed")
-            except Exception as e:
-                #print("Error occurred:", e)
-                #print("Error type:", type(e))
-                #print("Error arguments:", e.args)
-                print("pair error")
-                authorized = False
-                genChallenge() # updates pair_value
-                pair_characteristic.write(pair_value)
-                await asyncio.sleep_ms(100)
-
-        else:
-            await asyncio.sleep_ms(100)
-
-
-def on_validation_timer(timer):
-    #print("Validation timer expired, disconnecting.",timer)
-    asyncio.create_task(disconnect_device())
-
-async def disconnect_device():
-    global connected
-    global authorized
-    global currentConnection
-    if connected:
-        #print("Disconnecting:",currentConnection)
-        await currentConnection.disconnect(0)
-        #print("Disconnect complete")
 
 
 
@@ -428,12 +249,7 @@ async def disconnect_device():
 # connected.
 async def peripheral_task():
     global connected
-    global authorized
     global currentConnection
-    global validation_timer
-    global pair_value
-    global useValidation
-    validation_timer = machine.Timer(-1)
     print("Start adv")
     while True:
         try:
@@ -447,21 +263,16 @@ async def peripheral_task():
                 print("Connection from", connection.device,connection.device.addr_hex())
                 connected = True
                 currentConnection = connection
-                # new challenge
-                genChallenge() # initialize challenge
-                pair_characteristic.write(pair_value)
-                # Start a timer for client ID validation
-                if useValidation:
-                    validation_timer.init(mode=machine.Timer.ONE_SHOT, period=10000, callback=on_validation_timer)
-                # clear timer if authenticated like:
-                # validation_timer.deinit()  
+    ##            try:
+    ##                connection.pair("12341234")
+    ##                print("Pair OK")
+    ##            except Exception as e:
+    ##                print(f"Pair failed: {e}")
 
                 await connection.disconnected(timeout_ms=None)
                 print("Disconnected 1")
                 connected = False
-                authorized = False
                 currentConnection = None
-                asyncio.sleep_ms(10)
                 rgbFill((10,10,10))
         except:
             print("argh ...")
@@ -473,11 +284,10 @@ async def main():
     t1 = asyncio.create_task(sensor_task())
     t2 = asyncio.create_task(ctl_task())
     t3 = asyncio.create_task(peripheral_task())
-    t4 = asyncio.create_task(pair_task())
     # await asyncio.gather(t1, t2, t3)
 
     try:
-        await asyncio.gather(t1, t2, t3,t4)
+        await asyncio.gather(t1, t2, t3)
 
     except asyncio.TimeoutError:
         print("Timeout in main:")
